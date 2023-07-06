4285ae6dc26f334fc37d528cd6cfca788b7448ca
LucasDev <LucasDev9645@gmail.com>
2023-07-05 21:21:24 -0300

    Creating global reset, importing images, fonts and colors

---
+++ src/styles/styles.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const colors = {
+  black: "#111111",
+  white: "#FFFFFF",
+  red: "#E66767",
+  amber: "#FFEBD9",
+};
+
+export const GlobalCss = createGlobalStyle`
+* {
+ margin:0;
+ padding:0;
+ box-sizing:border-box;
+ font-family: 'Roboto', sans-serif;
+
+}
+
+h1, h2, h3, h4, h5 {
+  font-family: 'Zilla Slab', serif;
+}
+
+a {
+  text-decoration:none;
+}
+
+li {
+  list-style:none;
+}
+
+body {
+background-color:${colors.amber};
+color:${colors.red};
+}
+
+.container {
+  max-width: 64rem;
+  width: 100%;
+  margin: 0 auto;
+}
+
+`;
