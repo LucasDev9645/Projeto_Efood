1d4b220158d2fff91e1c9f6267592e476514adc3
LucasDev <LucasDev9645@gmail.com>
2023-07-20 20:27:32 -0300

    Create modal

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 
 import ProfileCard from "./ProfileCard";
 
+import PizzaImage from "../../assets/images/imageModal.png";
+import Close from "../../assets/images/close.png";
+
 import efoodLogo from "../../assets/images/logo.svg";
 import ImageProfileHeader from "../../assets/images/fundocurto.svg";
 import PresentationImage from "../../assets/images/imagemlimpa.png";
@@ -12,6 +15,13 @@ import {
   ImagePresentationHeader,
   ProfileMenuContainer,
 } from "./styles";
+import Button from "../Button";
+import {
+  ContainerModalCard,
+  ImgModal,
+  ModalCard,
+  ModalCardDescription,
+} from "./ProfileCard/styles";
 
 const Profile = () => {
   return (
@@ -59,6 +69,34 @@ const Profile = () => {
           <ProfileCard />
         </ul>
       </CardListContainer>
+      <ContainerModalCard>
+        <ModalCard>
+          <ImgModal>
+            <img src={PizzaImage} alt="imagem pizza" />
+          </ImgModal>
+          <ModalCardDescription>
+            <header>
+              <h3>Pizza Marguerita</h3>
+              <img src={Close} alt="imagem pizza" />
+            </header>
+            <p>
+              A pizza Margherita é uma pizza clássica da culinária italiana,
+              reconhecida por sua simplicidade e sabor inigualável. Ela é feita
+              com uma base de massa fina e crocante, coberta com molho de tomate
+              fresco, queijo mussarela de alta qualidade, manjericão fresco e
+              azeite de oliva extra-virgem. A combinação de sabores é perfeita,
+              com o molho de tomate suculento e ligeiramente ácido, o queijo
+              derretido e cremoso e as folhas de manjericão frescas, que
+              adicionam um toque de sabor herbáceo. É uma pizza simples, mas
+              deliciosa, que agrada a todos os paladares e é uma ótima opção
+              para qualquer ocasião.
+            </p>
+            <p>Serve: de 2 a 3 pessoas</p>
+            <Button name="Adicionar ao Carrinho - R$ 60,90" />
+          </ModalCardDescription>
+        </ModalCard>
+        <div className="overlay"></div>
+      </ContainerModalCard>
     </div>
   );
 };
